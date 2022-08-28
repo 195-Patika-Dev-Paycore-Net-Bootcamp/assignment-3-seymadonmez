5ad80898dca9334428c56e890040d3715b36d52a
Şeyma Dönmez <seymadonmezz1@gmail.com>
2022-08-28 04:04:01 +0300

    first commit

---
+++ WasteCollectionManagement/Context/IMapperSession.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using WasteCollectionManagement.Models;
+
+namespace WasteCollectionManagement.Context
+{
+    public interface IMapperSession
+    {
+        void BeginTransaction();
+        void Commit();
+        void Rollback();
+        void CloseTransaction();
+        void Save(Vehicle entity);
+        void Update(Vehicle entity);
+        void Delete(Vehicle entity);
+
+        IQueryable<Vehicle> Vehicles { get; }
+    }
+}
---
+++ WasteCollectionManagement/Controllers/VehicleController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WasteCollectionManagement.Context;
+using WasteCollectionManagement.Models;
+
+namespace WasteCollectionManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehicleController : ControllerBase
+    {
+        private readonly IMapperSession _session;
+        public VehicleController(IMapperSession session)
+        {
+            _session = session;
+        }
+
+        [HttpGet]
+        public List<Vehicle> Get()
+        {
+            List<Vehicle> result = _session.Vehicles.ToList();
+            return result;
+        }
+
+
+        [HttpGet("{id}")]
+        public Vehicle Get(int id)
+        {
+            Vehicle result = _session.Vehicles.Where(x => x.VehicleId == id).FirstOrDefault();
+            return result;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Vehicle vehicle)
+        {
+
+        }
+
+        [HttpPut]
+        public ActionResult<Vehicle> Put([FromBody] Vehicle request)
+        {
+
+            return Ok();
+        }
+
+
+        [HttpDelete("{id}")]
+        public ActionResult<Vehicle> Delete(int id)
+        {
+
+            return Ok();
+        }
+
+
+    }
+}
